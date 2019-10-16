2f208e65a9af6e858f24a824f4bd0a048fba353b
Luara Mineiro <luaramineiro@gmail.com>
2019-10-16 00:07:40 -0300

    change activity

--- app/src/main/java/com/luati/unicubos/MainActivity.kt
+++ app/src/main/java/com/luati/unicubos/MainActivity.kt
@@ -2,11 +2,39 @@ package com.luati.unicubos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.constraint_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.contraint_layout)
+        setContentView(R.layout.constraint_layout)
+
+        calculateBTN.setOnClickListener {
+            var heigth = heightEDTX.text.toString().toFloat()
+            var weight = weightEDTX.text.toString().toFloat()
+
+            if (heigth <= 0) {
+                Toast.makeText(this, "Digite um valor de altura > 0 ", Toast.LENGTH_LONG)
+                    .show()
+            }
+            else if (weight <= 0) {
+                Toast.makeText(this, "Digite um valor de peso > 0 ", Toast.LENGTH_LONG)
+                    .show()
+            }
+            else {
+                var imc = weight / (heigth * heigth)
+                Toast.makeText(this, "Seu IMC é igual a " + imc.toString(), Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        clearBTN.setOnClickListener {
+            heightEDTX.setText("")
+            weightEDTX.setText("")
+        }
     }
+
 }
