0721a1606ca6506520c0849184c25a54ba304a7a
Luara Mineiro <luaramineiro@gmail.com>
2019-10-15 22:58:08 -0300

    fix relative layout

--- app/src/main/java/com/luati/unicubos/MainActivity.kt
+++ app/src/main/java/com/luati/unicubos/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.relative_layout)
+        setContentView(R.layout.contraint_layout)
     }
 }
