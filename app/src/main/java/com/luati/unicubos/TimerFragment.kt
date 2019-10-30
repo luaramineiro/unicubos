236966007d6cb7721d59813ba785f0ae6872e187
Luara Mineiro <luara.mineiro@cubos.io>
2019-10-30 12:57:22 -0300

    layout dos fragments

--- app/src/main/java/com/luati/unicubos/TimerFragment.kt
+++ app/src/main/java/com/luati/unicubos/TimerFragment.kt
@@ -10,13 +10,13 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
-class StopWatchFragment : Fragment() {
+class TimerFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stop_watch, container, false)
+        return inflater.inflate(R.layout.fragment_timer, container, false)
     }
 }
